498e4a92fe0df6360c021ea89132db628e23dd42
manjula2107 <manjulait21@gmail.com>
2025-08-16 00:15:51 +0530

    Update transformations.sql

--- Scripts/transformations.sql
+++ Scripts/transformations.sql
@@ -15,8 +15,7 @@ JOIN snowflake_data_load.myschema.Orders o
     select * from snowflake_data_load.myschema.Customer_orders;
 
 //query the table customer_orders (final) table
-SELECT Customer_name,Email,Product,SUM(Total_amount) AS Total_spent
+SELECT Customer_name,SUM(Total_amount) AS Total_spent
 FROM snowflake_data_load.myschema.Customer_orders
-GROUP BY Customer_name,Email,Product
-ORDER BY Total_spent DESC;
-
+GROUP BY Customer_name
+ORDER BY Customer_name,Total_spent ASC;
